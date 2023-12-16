e020f96223760d119042d2800f6736ced88b80d6
SyedMuhammadJawwadNaqvi <144104325+SyedMuhammadJawwadNaqvi@users.noreply.github.com>
2023-12-16 18:29:46 +0500

    files uploaded

---
+++ script.js
@@ -0,0 +1,17 @@
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navbar = document.getElementsByClassName('nav-bar')[0]
+// const bar = document.getElementsByClassName('bar')[0]
+
+// toggleButton.addEventListener('click', () => {
+//   navbar.classList.toggle('active')
+//   toggleButton.classList.toggle('icon')
+// })
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbar = document.getElementsByClassName('nav-bar')[0]
+const bar = document.getElementsByClassName('bar')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbar.classList.toggle('active')
+  toggleButton.classList.toggle('icon')
+})
